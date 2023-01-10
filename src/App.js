21afd39b59f09e173cd505e147df390ed4b73545
Tehmoor Amjad <iamtehmooramjad@gmail.com>
2023-01-10 17:20:03 +0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import Expenses from "./components/expenses/Expenses";
+import React, { useState,useEffect } from 'react';
+import MainHeader from "./components/mainheader/header/Header";
+import Login from "./components/login/Login";
+import Home from "./components/home/Home";
 
 
-const App = () => {
+
+function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const  isLogIn= localStorage.getItem('isLoggedIn')
+        if (isLogIn === '1'){
+            setIsLoggedIn(true)
+        }
+    }, []);
+
+
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo any ways
+        localStorage.setItem('isLoggedIn','1')
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false);
+    };
+
     return (
-        <div className="App">
-
-            <Expenses/>
-        </div>
+        <React.Fragment>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler} />}
+                {isLoggedIn && <Home onLogout={logoutHandler} />}
+            </main>
+        </React.Fragment>
     );
 }
 
---
+++ src/components/mainheader/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import classes from './Header.module.css';
+import Navigation from "../navigation/Navigation";
+
+const Header = (props) => {
+    return (
+        <header className={classes['main-header']}>
+            <h1>A Typical Page</h1>
+            <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout}/>
+        </header>
+    );
+};
+
+export default Header;
